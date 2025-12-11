d7b0c0fc1c7174f10363d1590b33eeac52b97d33
wd56hp <will.darrah@darrahoil.com>
2025-12-10 21:54:44 -0600

    Refactor facility loading to dynamically fetch from GeoServer WFS, update map markers accordingly

--- web/js/map.js
+++ web/js/map.js
@@ -7,53 +7,85 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19
 }).addTo(map);
 
-// GeoServer WMS layer (uncomment and configure when ready)
-// const geoserverWMS = L.tileLayer.wms('http://172.28.0.20:8080/geoserver/aginfo/wms', {
-//     layers: 'aginfo:facilities',
-//     format: 'image/png',
-//     transparent: true,
-//     version: '1.1.0'
-// }).addTo(map);
+// GeoServer configuration - dynamically use current hostname
+const currentHost = window.location.hostname;
+const GEOSERVER_URL = `http://${currentHost}:8090/geoserver`;
+const WFS_LAYER = 'aginfo:facility';
 
-// Facility data
-const facilities = [
-    {
-        name: 'Alexander',
-        city: 'Alexander',
-        state: 'KS',
-        address: '200 W K-96',
-        lat: 38.469440,
-        lng: -99.553060
-    },
-    {
-        name: 'Bison',
-        city: 'Bison',
-        state: 'KS',
-        address: '100 North Main',
-        lat: 38.528900,
-        lng: -99.195400
-    }
-];
+// Store markers for potential cleanup
+let facilityMarkers = [];
 
-// Add markers for all facilities
-facilities.forEach(facility => {
-    const marker = L.marker([facility.lat, facility.lng])
-        .addTo(map)
-        .bindPopup(`<b>${facility.name}</b><br>${facility.city}, ${facility.state}<br>${facility.address}`);
-});
-
-// Function to load facilities from GeoServer or API
+// Function to load facilities dynamically from GeoServer WFS
 async function loadFacilities() {
-    // TODO: Fetch facilities from GeoServer WMS/WFS or REST API
-    // Example:
-    // const response = await fetch('http://172.28.0.20:8080/geoserver/aginfo/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=aginfo:facilities&outputFormat=application/json');
-    // const data = await response.json();
-    // Add markers or layers from the data
+    try {
+        // Build WFS GetFeature request URL
+        const wfsUrl = `${GEOSERVER_URL}/wfs?` +
+            `service=WFS&` +
+            `version=1.1.0&` +
+            `request=GetFeature&` +
+            `typeName=${WFS_LAYER}&` +
+            `outputFormat=application/json&` +
+            `srsName=EPSG:4326`;
+
+        console.log('Fetching facilities from:', wfsUrl);
+        
+        const response = await fetch(wfsUrl);
+        
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        
+        const geojson = await response.json();
+        
+        // Clear existing markers
+        facilityMarkers.forEach(marker => map.removeLayer(marker));
+        facilityMarkers = [];
+        
+        // Process GeoJSON features
+        if (geojson.features && geojson.features.length > 0) {
+            geojson.features.forEach(feature => {
+                const props = feature.properties;
+                const coords = feature.geometry.coordinates; // [lng, lat] in GeoJSON
+                
+                // Build popup content
+                const popupContent = `
+                    <b>${props.name || 'Unknown'}</b><br>
+                    ${props.city || ''}, ${props.state || ''}<br>
+                    ${props.address_line1 || ''}
+                    ${props.description ? '<br><small>' + props.description + '</small>' : ''}
+                `;
+                
+                // Create marker (note: GeoJSON uses [lng, lat], Leaflet uses [lat, lng])
+                const marker = L.marker([coords[1], coords[0]])
+                    .addTo(map)
+                    .bindPopup(popupContent);
+                
+                facilityMarkers.push(marker);
+            });
+            
+            console.log(`Loaded ${facilityMarkers.length} facilities from database`);
+            
+            // Fit map to show all facilities
+            if (facilityMarkers.length > 0) {
+                const group = new L.featureGroup(facilityMarkers);
+                map.fitBounds(group.getBounds().pad(0.1));
+            }
+        } else {
+            console.warn('No facilities found in database');
+        }
+    } catch (error) {
+        console.error('Error loading facilities:', error);
+        // Show error message to user
+        L.popup()
+            .setLatLng([38.5, -99.5])
+            .setContent(`<b>Error loading facilities</b><br>${error.message}<br><small>Check console for details</small>`)
+            .openOn(map);
+    }
 }
 
-// Load facilities on map ready
+// Load facilities when map is ready
 map.whenReady(() => {
-    console.log('Map ready - showing', facilities.length, 'facilities');
-    // loadFacilities();
+    console.log('Map ready - loading facilities from GeoServer...');
+    loadFacilities();
 });
 
